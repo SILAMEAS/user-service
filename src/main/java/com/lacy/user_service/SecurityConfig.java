c579eccf04c12fcb894732d01343b8b55b1c6731
Sila MEAS <sila.meas@allweb.com.kh>
2025-07-09 13:46:57 +0700

    working with auth0

--- src/main/java/com/lacy/user_service/SecurityConfig.java
+++ src/main/java/com/lacy/user_service/SecurityConfig.java
@@ -1,16 +1,50 @@
 package com.lacy.user_service;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.endpoint.RestClientAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.HttpSessionOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizationRequestResolver;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.CompositeLogoutHandler;
+import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public SecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientRepository repository() {
+        return new HttpSessionOAuth2AuthorizedClientRepository();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,12 +53,55 @@ public class SecurityConfig {
                         .requestMatchers("/public/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .oauth2Login(oauth2 -> oauth2
+                        .successHandler(new AuthenticationSuccessHandler() {
+                            @Override
+                            public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                                response.sendRedirect("http://localhost:4000?access_token=" + repository().loadAuthorizedClient("auth0", authentication, request).getAccessToken().getTokenValue());
+                            }
+                        })
+                )
+                .logout(logout -> {
+                            LogoutHandler logoutHandler = new CookieClearingLogoutHandler("*");
+                            SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+                            CompositeLogoutHandler compositeLogoutHandler = new CompositeLogoutHandler(logoutHandler, securityContextLogoutHandler);
+
+                            LogoutSuccessHandler logoutSuccessHandler = new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+                            logout
+                                    .addLogoutHandler(compositeLogoutHandler)
+                                    .logoutSuccessHandler(logoutSuccessHandler);
+                        }
+                )
                 .oauth2ResourceServer(oauth2 -> oauth2
                         .jwt(jwt -> jwt
                                 .decoder(jwtDecoder())
                         )
                 );
         return http.build();
+    }
+
+    @Bean
+    public OAuth2AuthorizationRequestResolver authRequestResolver() {
+        var defaultResolver = new DefaultOAuth2AuthorizationRequestResolver(
+                clientRegistrationRepository, "/oauth2/authorization");
+
+        defaultResolver.setAuthorizationRequestCustomizer(customizer ->
+                customizer.additionalParameters(params ->
+                        params.put("audience", "https://dev-p0goqmyozbhmcoys.us.auth0.com/api/v2/")
+                )
+        );
+
+        return defaultResolver;
+    }
+
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+        RestClientAuthorizationCodeTokenResponseClient client = new RestClientAuthorizationCodeTokenResponseClient();
+        client.setParametersCustomizer(params -> {
+            params.add("audience", "https://dev-p0goqmyozbhmcoys.us.auth0.com/api/v2/");
+        });
+        return client;
     }
 
     @Bean
